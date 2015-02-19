9e2de08de2eb8ced429ec86b1278cf26b4976e1e
Robert D'Arrigo <robert@funkelabs.com.au>
2015-02-19 16:57:15 +1100

    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,5 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation and there may be some benefit to caching the inverse of a matrix rather than compute it repeatedly
+## The following pair of functions creates a special "matrix" object that can cache it's inverse and calculates the inverse then stores it in the objects cache
 
 ## This function creates a special "matrix" object that can cache its inverse.
 
